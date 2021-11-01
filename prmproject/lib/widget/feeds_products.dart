03fa82bc53c6181f596dc1d058a802eaa9665a63
Le Anh Nguyen <nguyenlase141102@fpt.edu.vn>
2021-11-01 23:06:45 +0700

    d

--- prmproject/lib/widget/feeds_products.dart
+++ prmproject/lib/widget/feeds_products.dart
@@ -39,7 +39,7 @@ class _FeedProductsState extends State<FeedProducts> {
                         borderRadius: BorderRadius.circular(2),
                         child: Container(
                           width: double.infinity,
-                          height: MediaQuery.of(context).size.height * 0.3,
+                          height: MediaQuery.of(context).size.height * 0.2,
                           child: Image.network(
                             productsAttributes.imageUrl,
                             //   fit: BoxFit.fitWidth,
@@ -57,7 +57,7 @@ class _FeedProductsState extends State<FeedProducts> {
                           badgeColor: Colors.pink,
                           borderRadius: BorderRadius.only(
                               bottomRight: Radius.circular(8)),
-                          badgeContent: Text('New',
+                          badgeContent: Text('Sản phẩm mới',
                               style: TextStyle(color: Colors.white)),
                         ),
                       ),
@@ -71,26 +71,36 @@ class _FeedProductsState extends State<FeedProducts> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(
-                      height: 4,
-                    ),
-                    Text(
-                      productsAttributes.description,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                      style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w600),
-                    ),
+                    SizedBox(height: 10),
+                    Text('${productsAttributes.title}',style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                    )),
+                    SizedBox(height: 10),
+                        Text(
+                          'Loại sản phẩm : ${productsAttributes.productCategoryName}',
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w600),
+                        ),
+                         SizedBox(height: 10),
+                        Text(
+                          'Số lượng : ${productsAttributes.quantity}',
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w600),
+                        ),
                     Padding(
                       padding: const EdgeInsets.symmetric(vertical: 8.0),
                       child: Text(
-                        '\$ ${productsAttributes.price}',
+                        'Giá : ${productsAttributes.price}',
                         overflow: TextOverflow.ellipsis,
                         maxLines: 2,
                         style: TextStyle(
-                            fontSize: 18,
+                            fontSize: 14,
                             color: Colors.black,
                             fontWeight: FontWeight.w900),
                       ),
@@ -99,12 +109,13 @@ class _FeedProductsState extends State<FeedProducts> {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(
-                          '${productsAttributes.quantity}',
+                          'Mô tả : ${productsAttributes.description}',
                           style: TextStyle(
                               fontSize: 12,
                               color: Colors.grey,
                               fontWeight: FontWeight.w600),
                         ),
+                        
                         Material(
                           color: Colors.transparent,
                           child: InkWell(
